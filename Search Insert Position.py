cd18525c9d43288caffdfb7f78ac847c70f0c565
Ayaan Atif <140044111+ayaanatifcodes@users.noreply.github.com>
2025-12-22 19:47:53 +0500

    Add files via upload

---
+++ Search Insert Position.py
@@ -0,0 +1,16 @@
+class Solution:
+    def searchInsert(self, nums: list[int], target: int) -> int:
+        first = 0
+        last = len(nums) - 1
+        while first <= last:
+            mid = (first + last) //2
+
+
+            if nums[mid] == target:
+                return mid
+            else:
+                if nums[mid] < target:
+                    first = mid + 1
+                else:
+                    last = mid - 1
+        return first
