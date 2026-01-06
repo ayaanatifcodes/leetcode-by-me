8144d51295232a5345fc215ac309f8c078bb6234
Ayaan Atif <140044111+ayaanatifcodes@users.noreply.github.com>
2026-01-06 22:21:17 +0500

    Rename result variable to results for clarity

--- kids-with-the-greatest-number-of-candies.py
+++ kids-with-the-greatest-number-of-candies.py
@@ -1,9 +1,10 @@
 from typing import List
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
-        result = []
+        results = []
         maxCandies = max(candies)
         for candy in candies:
-            result.append(candy + extraCandies >= maxCandies)
-        return result
+            results.append(candy + extraCandies >= maxCandies)
+        return results
+
 
