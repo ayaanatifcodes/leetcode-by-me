31ec9ffca35fb69cd4ee262d1a1b51ce3b7e51d1
Ayaan Atif <140044111+ayaanatifcodes@users.noreply.github.com>
2026-01-09 18:30:23 +0500

    Update can-place-flowers.py

--- can-place-flowers.py
+++ can-place-flowers.py
@@ -4,11 +4,9 @@ class Solution:
         for i in range(len(flowerbed)):
             if n == 0:
                 return True
-
             if flowerbed[i] == 0:
                 left_empty = (i == 0 or flowerbed[i - 1] == 0)
                 right_empty = (i == len(flowerbed) - 1 or flowerbed[i + 1] == 0)
-
                 if left_empty and right_empty:
                     flowerbed[i] = 1
                     n -= 1
