71c4411465f39b5b2e0002e42e067d32ce3a0991
Ayaan Atif <140044111+ayaanatifcodes@users.noreply.github.com>
2026-01-15 15:44:03 +0500

    Update reverse-words-in-a-string.py

--- reverse-words-in-a-string.py
+++ reverse-words-in-a-string.py
@@ -1,12 +1,10 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        new_s = s.strip() 
-        new_s = new_s.split()   
+        new_s = s.strip()
+        new_s = new_s.split() 
         length = len(new_s) 
-        
-        for i in range(length // 2):
-            extra_s = new_s[i]
+        for i in range(length // 2): 
+            extra_s = new_s[i] 
             new_s[i] = new_s[length - 1 - i]
             new_s[length - 1 - i] = extra_s
-            
         return " ".join(new_s)
