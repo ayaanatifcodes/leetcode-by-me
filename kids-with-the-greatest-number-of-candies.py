afbcdfb47984cf17f62f830a76cdbaf1442bea34
Ayaan Atif <140044111+ayaanatifcodes@users.noreply.github.com>
2026-01-16 18:56:06 +0500

    Update kids-with-the-greatest-number-of-candies.py

--- kids-with-the-greatest-number-of-candies.py
+++ kids-with-the-greatest-number-of-candies.py
@@ -1,9 +1,8 @@
 from typing import List
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        results = []
         maxCandies = max(candies)
-        result = []
-
         for candy in candies:
-            result.append(candy + extraCandies >= maxCandies)
-        return result
+            results.append(candy + extraCandies >= maxCandies)
+        return results 
