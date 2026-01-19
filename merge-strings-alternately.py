ecded573382734c9cc1f00dc92995b55cdc23b1a
Ayaan Atif <140044111+ayaanatifcodes@users.noreply.github.com>
2026-01-19 16:33:21 +0500

    Update merge-strings-alternately.py

--- merge-strings-alternately.py
+++ merge-strings-alternately.py
@@ -1,6 +1,6 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        new_str = ''
+        new_str = ""
         min_length = min(len(word1), len(word2))
         for i in range(min_length):
             new_str += word1[i]
@@ -9,4 +9,4 @@ class Solution:
             new_str += word1[min_length:]
         else:
             new_str += word2[min_length:]
-        return new_str
+        return new_str  
