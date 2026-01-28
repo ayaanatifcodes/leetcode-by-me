028119c434b758d7afc32b6f43abebeba65b16ad
Ayaan Atif <140044111+ayaanatifcodes@users.noreply.github.com>
2026-01-28 12:57:04 +0500

    Rename result variable to results for clarity

--- product-of-array-except-self.py
+++ product-of-array-except-self.py
@@ -1,13 +1,13 @@
 from typing import List
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        result = [1] * len(nums)
+        results = [1] * len(nums)
         prefix = 1
         for i in range(len(nums)):
-            result[i] = prefix
+            results[i] = prefix
             prefix *= nums[i]
         postfix = 1
         for i in range(len(nums) - 1, -1, -1):
-            result[i] *= postfix
+            results[i] *= postfix
             postfix *= nums[i]
-        return result
+        return results
