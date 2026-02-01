f45c29ed7a8a14155b353171acf031134f0f2738
Ayaan Atif <140044111+ayaanatifcodes@users.noreply.github.com>
2026-02-01 13:09:44 +0500

    Update can-place-flowers.py

--- can-place-flowers.py
+++ can-place-flowers.py
@@ -1,12 +1,13 @@
+from typing import List
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
         for i in range(len(flowerbed)):
             if n == 0:
                 return True
             if flowerbed[i] == 0:
-                left_empty = (i == 0 or flowerbed[i - 1] == 0 )
-                right_empty = (i == len(flowerbed) - 1 or flowerbed[i + 1] == 0)
-                if left_empty and right_empty:
+                left_side_empty = (i == 0 or flowerbed[i - 1] == 0)
+                right_side_empty = (i == len(flowerbed) - 1 or flowerbed[i + 1] == 0)
+                if left_side_empty and right_side_empty:
                     flowerbed[i] = 1
                     n -= 1
         return n <= 0
