0e090fa4cecc23e5513f80e2f734e3e14473fd9a
Ayaan Atif <140044111+ayaanatifcodes@users.noreply.github.com>
2026-02-01 12:58:29 +0500

    Update greatest-common-divisor-of-strings.py

--- greatest-common-divisor-of-strings.py
+++ greatest-common-divisor-of-strings.py
@@ -3,7 +3,7 @@ class Solution:
         if str1 + str2 != str2 + str1:
             return ""
         if len(str1) == len(str2):
-            return str1
+            return str2
         if len(str1) > len(str2):
             return self.gcdOfStrings(str1[len(str2):], str2)
         return self.gcdOfStrings(str2[len(str1):], str1)
