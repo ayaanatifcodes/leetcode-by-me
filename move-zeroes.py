86a1b025fa553bf6dff19195ae7bb63ed57c71c9
Ayaan Atif <140044111+ayaanatifcodes@users.noreply.github.com>
2026-02-03 19:55:17 +0500

    Add files via upload

---
+++ move-zeroes.py
@@ -0,0 +1,11 @@
+from typing import List
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        num_array = []
+        zero_array = []
+        for num in nums:
+            if num != 0:
+                num_array.append(num)
+            else:
+                zero_array.append(num)
+        nums[:] = num_array + zero_array
