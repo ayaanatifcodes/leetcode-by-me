88e33ee82032d31dcec42252fc3234782173f4b1
Ayaan Atif <140044111+ayaanatifcodes@users.noreply.github.com>
2026-02-04 12:33:58 +0500

    Update reverse-words-in-a-string.py

--- reverse-words-in-a-string.py
+++ reverse-words-in-a-string.py
@@ -4,8 +4,8 @@ class Solution:
         new_s = s.strip()
         new_s = new_s.split()
         s_length = len(new_s)
-        for i in range(s_length // 2):
-            extra_s = new_s[i]
-            new_s[i] = new_s[s_length - 1 - i]
-            new_s[s_length - 1 - i] = extra_s
-        return " ".join(new_s)
+        for num in range(s_length // 2):
+            extra_s = new_s[num]
+            new_s[num] = new_s[s_length - 1 - num]
+            new_s[s_length - 1 - num] = extra_s
+        return" ".join(new_s)
