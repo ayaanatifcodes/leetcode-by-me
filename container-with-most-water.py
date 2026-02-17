869adc6b4423709eb128a449f57de448d48cc053
Ayaan Atif <140044111+ayaanatifcodes@users.noreply.github.com>
2026-02-17 12:37:18 +0500

    Update container-with-most-water.py

--- container-with-most-water.py
+++ container-with-most-water.py
@@ -4,9 +4,9 @@ class Solution:
         j = len(height) - 1
         results = 0
         while i < j:
-            results = max(results, (j - i) * min(height[i], height[j]))
+            result = max(result, (j - i) * min(height[i], height[j]))
             if height[i] < height[j]:
                 i += 1
             else:
                 j -= 1
-        return results
+        return result
