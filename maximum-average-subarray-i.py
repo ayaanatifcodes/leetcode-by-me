7126b100608a26e968fbafa2e23052f12e346cf8
Ayaan Atif <140044111+ayaanatifcodes@users.noreply.github.com>
2026-02-21 13:02:18 +0500

    Update maximum-average-subarray-i.py

--- maximum-average-subarray-i.py
+++ maximum-average-subarray-i.py
@@ -5,8 +5,9 @@ class Solution:
         window_sum = sum(nums[left:right]) # nums[start:end] so for this 3 elements are covered
         max_sum = window_sum
         while right < len(nums):
-            window_sum = window_sum - nums[left] + nums[right]
+            window_sum = window_sum - nums[left] + nums[right] # Using sliding window to update current value
             max_sum = max(max_sum, window_sum)
             left += 1
             right += 1
         return max_sum / k
+
