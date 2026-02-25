6b7bb2a33382e460419dfd5731e856ab9ef9073d
Ayaan Atif <140044111+ayaanatifcodes@users.noreply.github.com>
2026-02-25 14:43:09 +0500

    Update length-of-last-word.py

--- length-of-last-word.py
+++ length-of-last-word.py
@@ -2,7 +2,7 @@ class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         s = s.rstrip()
         word_length = 0
-        for i in range (len(s)-1, -1, -1):
+        for i in range (len(s) - 1, -1, -1):
             if s[i] == ' ':
                 break
             word_length += 1
