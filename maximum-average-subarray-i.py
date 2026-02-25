b6665c0fbc9acd3edeea9ed127cd4e71264cf387
Ayaan Atif <140044111+ayaanatifcodes@users.noreply.github.com>
2026-02-25 14:42:52 +0500

    Update maximum-average-subarray-i.py

--- maximum-average-subarray-i.py
+++ maximum-average-subarray-i.py
@@ -2,10 +2,10 @@ class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
         left = 0
         right = k
-        window_sum = sum(nums[left:right]) # nums[start:end] so for this 3 elements are covered
+        window_sum = sum(nums[left:right])
         max_sum = window_sum
         while right < len(nums):
-            window_sum = window_sum - nums[left] + nums[right] # Using sliding window to update current value
+            window_sum = window_sum - nums[left] + nums[right]
             max_sum = max(max_sum, window_sum)
             left += 1
             right += 1
