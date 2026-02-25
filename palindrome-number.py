e7e9fe82143fcb877004ef2a4ae49ecf01b5a3a4
Ayaan Atif <140044111+ayaanatifcodes@users.noreply.github.com>
2026-02-25 14:42:25 +0500

    Update palindrome-number.py

--- palindrome-number.py
+++ palindrome-number.py
@@ -2,8 +2,8 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x < 0:  
             return False
-        string = str(x)
-        reversed_string = ""
+        str = str(x)
+        reversed_str = ""
         for i in range(len(s) - 1, -1, -1):
-            reversed_string += s[i]
+            reversed_str += s[i]
         return str == reversed_str
