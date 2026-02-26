f1a84fe1c28aa8202cdea5323631a236ef0c06e4
Ayaan Atif <140044111+ayaanatifcodes@users.noreply.github.com>
2026-02-26 14:56:42 +0500

    Update removing-stars-from-a-string.py

--- removing-stars-from-a-string.py
+++ removing-stars-from-a-string.py
@@ -1,9 +1,9 @@
 class Solution(object):
     def removeStars(self, s: str) -> str:
-        s_stack = []
+        stack = []
         for i in s:
             if i != '*':
-                s_stack.append(i)
+                stack.append(i)
             else:
-                s_stack.pop()
-        return''.join(s_stack)
+                stack.pop()
+        return''.join(stack)
