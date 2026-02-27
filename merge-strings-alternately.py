a080eed271fe2d7c7ca2849d089a1a39bc3efdd3
Ayaan Atif <140044111+ayaanatifcodes@users.noreply.github.com>
2026-02-27 22:55:38 +0500

    Update merge-strings-alternately.py

--- merge-strings-alternately.py
+++ merge-strings-alternately.py
@@ -1,12 +1,13 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        new_string = ""
+        string = ""
         min_length = min(len(word1), len(word2))
         for i in range(min_length):
-            new_string += word1[i]
-            new_string += word2[i]
+            string += word1[i]
+            string += word2[i]
         if len(word1) > len(word2):
-            new_string += word1[min_length:]
+            string += word1[min_length:]
         else:
             string += word2[min_length:]
         return string
+
