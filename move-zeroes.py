c333d8dfa46e2da823bdf6d11b2079385708ab1e
Ayaan Atif <140044111+ayaanatifcodes@users.noreply.github.com>
2026-03-01 14:10:19 +0500

    Update move-zeroes.py

--- move-zeroes.py
+++ move-zeroes.py
@@ -2,10 +2,10 @@ from typing import List
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         num_array = []
-        zeroes_array = []
+        zero_array = []
         for num in nums:
             if num != 0:
                 num_array.append(num)
             else:
-                zeroes_array.append(num)
+                zero_array.append(num)
         nums[:] = num_array + zeroes_array
