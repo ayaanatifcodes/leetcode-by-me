94b61a30f2042ac309bc658a14880b3632b5c33d
Ayaan Atif <140044111+ayaanatifcodes@users.noreply.github.com>
2026-03-03 12:56:29 +0500

    Update asteroid-collision.py

--- asteroid-collision.py
+++ asteroid-collision.py
@@ -1,7 +1,6 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
         stack = []  # This will store asteroids that are still moving safely
-
         for asteroid in asteroids:
             # Check for a collision:
             # Collision only happens if:
@@ -9,22 +8,19 @@ class Solution:
             # 2) Current asteroid is moving left (negative)
             # 3) Top of stack is moving right (positive)
             while stack and asteroid < 0 and stack[-1] > 0:
-                
                 # If the top asteroid is smaller, it explodes
                 if stack[-1] < -asteroid:
                     stack.pop()
                     continue  # Keep checking for more possible collisions
-                
                 # If both are equal size, both explode
                 elif stack[-1] == -asteroid:
                     stack.pop()
-                
                 # In both equal or larger-top cases, stop checking
-                break
-            
+                break            
             else:
                 # This else runs only if the while loop did NOT break
                 # Meaning the asteroid survived all collisions
                 stack.append(asteroid)
 
         return stack  # Remaining asteroids after all collisions
+
