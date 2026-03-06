a539a4942fd04baeb7108856bb80acb2419ffbb9
Ayaan Atif <140044111+ayaanatifcodes@users.noreply.github.com>
2026-03-06 14:06:31 +0500

    Update delete-the-middle-node-of-a-linked-list.py

--- delete-the-middle-node-of-a-linked-list.py
+++ delete-the-middle-node-of-a-linked-list.py
@@ -3,13 +3,12 @@ class Solution:
         if not head or not head.next:
             return None
         prev, slow, fast = None, head, head
-        
         while fast and fast.next:
             prev = slow
             slow = slow.next
             fast = fast.next.next
-        
         if prev:
             prev.next = slow.next
 
         return head
+
