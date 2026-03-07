621a8ac4a49839c4b3072b182c1b6effe7f1f110
Ayaan Atif <140044111+ayaanatifcodes@users.noreply.github.com>
2026-03-07 13:37:14 +0500

    Update kids-with-the-greatest-number-of-candies.py

--- kids-with-the-greatest-number-of-candies.py
+++ kids-with-the-greatest-number-of-candies.py
@@ -6,16 +6,3 @@ class Solution:
         for candy in candies:
             result.append(candy + extraCandies >= maxCandies)
         return result
-
-#  * @param {number[]} candies
-#  * @param {number} extraCandies
-#  * @return {boolean[]}
-#  */
-# var kidsWithCandies = function(candies, extraCandies) {
-#     let results = []
-#     let max_Candies = Math.max(...candies)
-#
-#     for(let i = 0; i < candies.length; i++) {
-#         results[i] = candies[i] + extraCandies >= max_Candies
-#     }
-#     return results
