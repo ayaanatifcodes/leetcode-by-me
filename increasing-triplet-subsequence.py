582e8520627cab025d4bb73a186e96ff6428a156
Ayaan Atif <140044111+ayaanatifcodes@users.noreply.github.com>
2026-03-29 15:31:19 +0500

    Implement increasingTriplet function in Solution class

--- increasing-triplet-subsequence.py
+++ increasing-triplet-subsequence.py
@@ -1,15 +1,13 @@
+from typing import List
 class Solution:
-    def deleteMiddle(self, head: ListNode) -> ListNode:
-        if not head or not head.next:
-            return None
-        count = 0
-        temp = head
-        while temp:
-            count += 1
-            temp = temp.next
-        mid = count // 2
-        temp = head
-        for _ in range(mid - 1):
-            temp = temp.next
-        temp.next = temp.next.next
-        return head
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        first = float('inf')
+        second = float('inf')
+        for num in nums:
+            if num <= first:
+                first = num
+            elif num <= second:
+                second = num
+            else:
+                return True
+        return False
