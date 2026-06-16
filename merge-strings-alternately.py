63de430755cbb26a0b6390d2b23e1830fafd1508
Ayaan Atif <140044111+ayaanatifcodes@users.noreply.github.com>
2026-06-16 17:41:27 +0500

    Refactor mergeAlternately function in Python

--- merge-strings-alternately.py
+++ merge-strings-alternately.py
@@ -1,15 +1,14 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        string = ""
-        min_length = min(len(word1), len(word2))
-        for i in range(min_length):
-            string += word1[i]
-            string += word2[i]
+        newstr = ""
+        for i in range(min(len(word1), len(word2))):
+            newstr += word1[i]
+            newstr += word2[i]
         if len(word1) > len(word2):
-            string += word1[min_length:]
+            newstr += word1[len(word2):]
         else:
-            string += word2[min_length:]
-        return string
+           newstr += word2[len(word1):]
+        return newstr
 
 # Javascript Approach        
 # /**
