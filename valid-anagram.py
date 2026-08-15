3a093c633d8c1bea514732e416a25783f9961873
Ayaan Atif <140044111+ayaanatifcodes@users.noreply.github.com>
2026-08-15 14:27:14 +0500

    Update valid-anagram.py

--- valid-anagram.py
+++ valid-anagram.py
@@ -1,9 +1,6 @@
+from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        else:
-            for char in s:
-                if s.count(char) != t.count(char):
-                    return False
-            return True
+        return Counter(s) == Counter(t)
